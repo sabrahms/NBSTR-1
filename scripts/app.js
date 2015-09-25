135925126cb806ba617ed89a6d9e4afa773b4dc6
Sam Abrahms <abrahms.sam@gmail.com>
2015-09-25 16:37:57 -0500

    added bootstrap and font awesome as well as bits of content

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+var app = angular.module("newBeginnings", ["ui.router"]);
+
+app.config(function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+  requireBase: false
+    });
+    
+    $stateProvider
+        .state("main", {
+        url: "/",
+        
+        templateUrl: "/templates/home.html"
+    })
+        .state("dogs", {
+        url: "/dogs",
+        
+        templateUrl: "/templates/dogs.html"
+    })
+        .state("donate", {
+        url: "/donate",
+        
+        templateUrl: "/templates/donate.html"
+    })
+    
+        .state("volunteer", {
+        url: "/volunteer",
+        
+        templateUrl: "/templates/volunteer.html"
+    })
+        .state("events", {
+        url: "/events",
+        
+        templateUrl: "/templates/events.html"
+    })
+        .state("about", {
+        url: "/about",
+        
+        templateUrl: "/templates/about.html"
+    })
+        .state("surrender", {
+        url: "/owner-surrender",
+        
+        templateUrl: "/templates/surrender.html"
+    });
+});
+
+
+               
+               
+               
