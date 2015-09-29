e5db4edf3125e29bd27dcd4d2abaf678ba6c79d5
Sam Abrahms <abrahms.sam@gmail.com>
2015-09-29 17:06:04 -0500

    most of the content added, minus a few sections

--- scripts/app.js
+++ scripts/app.js
@@ -14,38 +14,26 @@ app.config(function($stateProvider, $locationProvider) {
     })
         .state("dogs", {
         url: "/dogs",
-        
         templateUrl: "/templates/dogs.html"
     })
         .state("donate", {
         url: "/donate",
-        
         templateUrl: "/templates/donate.html"
     })
-    
         .state("volunteer", {
         url: "/volunteer",
-        
         templateUrl: "/templates/volunteer.html"
     })
         .state("events", {
         url: "/events",
-        
         templateUrl: "/templates/events.html"
     })
         .state("about", {
         url: "/about",
-        
         templateUrl: "/templates/about.html"
     })
         .state("surrender", {
         url: "/owner-surrender",
-        
         templateUrl: "/templates/surrender.html"
     });
 });
-
-
-               
-               
-               
